48506aec8510a221436b423554631fcf92338419
Gniare <niare09@hotmail.fr>
2024-02-27 16:03:33 +0100

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit -ichecks for a digit (0 through 9)
+ * @c: int type digit
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
