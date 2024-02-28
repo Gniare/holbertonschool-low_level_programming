6c28f8ada28428b912b449ba80c1f4759b8430c7
Gniare <niare09@hotmail.fr>
2024-02-28 09:22:15 +0100

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- *Function to print numbers from 0 to 9 excluding 2 and 4 
- *use _putchar twice in your code
+ * print_most_numbers - Print numbers from 0 to 9 excluding 2 and 4 
+ * Return: void
 */
-
 void print_most_numbers(void)
 {
     int i;
 
-    for (i = 0; i < 10; i ++)
+    for (i = 0; i < 10; i++)
     {
         if (i != 2 && i != 4)
         {
             _putchar (i + '0');
         }
     }
-_putchar('\n');
+    _putchar('\n');
 }
