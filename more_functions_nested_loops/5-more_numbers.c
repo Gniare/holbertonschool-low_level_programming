a1cfa62b83509de36dedc9b36cae584050b05755
Gniare <niare09@hotmail.fr>
2024-02-28 11:12:02 +0100

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * void more_numbers - prints 10 times the numbers, from 0 to 14
- * use _putchar three times
- * _putchar to print the numbers
- * return void 
- */
+* void more_numbers - prints 10 times the numbers, from 0 to 14
+* use _putchar three times
+* _putchar to print the numbers
+* return void 
+*/
 void more_numbers(void)
 {
-	int i, j;
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 0; i <= 14; i++)
-		{
-			_putchar(i + '0');
-			if (i < 14)
-			{
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+int i, j;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i + '0');
+if (i < 14)
+{
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
