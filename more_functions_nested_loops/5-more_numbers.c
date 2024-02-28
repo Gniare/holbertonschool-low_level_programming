34258274267d6320e60c962fafe3f1264d3e8ac0
Gniare <niare09@hotmail.fr>
2024-02-28 14:15:42 +0100

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
 *more_numbers - prints 10 times the numbers, from 0 to 14
-*Return: void 
+*Return: void
 */
 void more_numbers(void)
 {
@@ -15,7 +15,7 @@ for (i = 0; i <= 14; i++)
 
 if (i > 9)
 {
-_putchar((i / 10) +'0');
+_putchar((i / 10) + '0');
 }
 
 _putchar((i % 10) + '0');
