0e742f704bea25d5d767dd4b4a6c7833426a5499
Gniare <niare09@hotmail.fr>
2024-03-03 22:38:59 +0100

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,10 @@ void puts2(char *str)
 {
 int i;
 
-i = 0; /* initialise compteur a 0 pour commencer au first caractere */
-while (str[i] != '\0') /* boucle parcours la chaine de caractere */
+i = 0;
+while (str[i] != '\0')
 {
-if (i % 2 == 0) /
+if (i % 2 == 0)
 {
 _putchar(str[i]);
 }
