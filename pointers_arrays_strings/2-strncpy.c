13d5b511c406adf169b91cbf431f5dee687383d2
Gniare <niare09@hotmail.fr>
2024-03-03 23:55:03 +0100

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strlen - returns the length of a string
+*@s: string
+*Return: length
+*/
+
+int _strlen(char *s)
+{
+int len = 0;
+
+for (; *s != '\0'; s++)
+{
+len++;
+}
+
+return (len);
+}
