03448c9d0170516ae6f346b3b85f66415d8f4aff
Gniare <niare09@hotmail.fr>
2024-03-05 00:03:19 +0100

    2-strchr.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memcpy - copies memory area
+*@dest: destinations memory area
+*src: sources memory area
+*@n: number of byres
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+dest++;
+}
+return (dest);
+}
