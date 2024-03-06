642c2a39e39b7fdaf9987cd3ada7a7781e563bd5
Gniare <niare09@hotmail.fr>
2024-03-06 11:51:12 +0100

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
-*_strspn - The function is used to calculate the length of the initial segment of a string consisting of only the characters that are present in another specified string.
-*@s:the number of bytes in the initial 
+*_strspn - The function is used to calculate the length of the initial segment of a string consisting of only the characters that are present in another specified string
+*@s:the number of bytes in the initial
 *Return: s
 */
 
@@ -12,7 +12,6 @@ unsigned int _strspn(char *s, char *accept)
 int i = 0;
 int j;
 int h = 0;
-
 while (s[i] != '\0')
 {
 for (j = 0; accept[j] != '\0'; j++)
@@ -24,7 +23,6 @@ break;
 }
 if (accept[j + 1] == '\0' && s[i] != accept[i])
 return (h);
-}
 i++;
 }
 return (h);
