677660b5928be9175bf6e2a3f673c2034d17fd16
Gniare <niare09@hotmail.fr>
2024-03-06 11:07:57 +0100

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,12 @@
-/*
- * File: 2-strchr.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to be searched.
- * @c: The character to be located.
- *
- * Return: If c is found - a pointer to the first occurence.
- *         If c is not found - NULL.
- */
+*_strchr - Locates a character in a string.
+*@s: The string to be searched.
+*@c: The character to be located.
+*Return: If c is found - a pointer to the first occurence.
+*/
+
 char *_strchr(char *s, char c)
 {
 int index;
