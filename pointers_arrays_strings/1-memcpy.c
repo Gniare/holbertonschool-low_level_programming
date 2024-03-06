b15b2e14ade5fdf74dca1a4a1046153a0ac076fc
Gniare <niare09@hotmail.fr>
2024-03-06 01:59:36 +0100

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
-* _memcpy - copies memory area
-*@dest: destinations memory area
-*src: sources memory area
-*@n: number of byres
+*_memcpy - copies memory area
+*@dest:destinations memory area
+*src:sources memory area
+*@n:number of byres
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
-
-
 {
 unsigned int i;
 for (i = 0; i < n; i++)
