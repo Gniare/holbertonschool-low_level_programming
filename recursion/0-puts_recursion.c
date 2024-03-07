410eaf1548fd8a94aaef1a7286128b15065169cd
Gniare <niare09@hotmail.fr>
2024-03-07 13:59:16 +0100

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,19 +2,17 @@
 
 /**
 *_puts_recursion - prints a string, followed by new line
-*@s: string to print
+*@s:string to print
 */
-
-#include <stdio.h>
 
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
 {
-putchar('\n');
+_putchar('\n');
 return;
 }
-putchar(*s);
-
-_puts_recursion(s + 1);
+_putchar(*s);
+s++;
+_puts_recursion(s);
 }
