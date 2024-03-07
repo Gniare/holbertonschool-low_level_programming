84385f87b1e860482e16accd737bd3d3233104b1
Gniare <niare09@hotmail.fr>
2024-03-07 10:30:32 +0100

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -3,17 +3,20 @@
 /**
 *_memcpy - copies memory area
 *@dest:destinations memory area
-*src:sources memory area
+*@src:sources memory area
 *@n:number of byres
+*Return: ptr
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
+char *ptr = dest;
+
 for (i = 0; i < n; i++)
 {
 *dest = src[i];
 dest++;
 }
-return (dest);
+return (ptr);
 }
