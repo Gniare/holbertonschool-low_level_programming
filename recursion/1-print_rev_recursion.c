14b548e62a7ca6ab22ac91ffe332ed5f1b94cfdd
Gniare <niare09@hotmail.fr>
2024-03-07 14:59:37 +0100

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,11 +3,15 @@
 
 /**
 *_print_rev_recursion - a function that prints a string in reverse
-*
+*@s:string to print
 */
 
 void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
+{
 return;
+}
+_print_rev_recursion(s + 1);
+printf("%c", *s);
 }
