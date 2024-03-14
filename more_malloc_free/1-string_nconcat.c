b878b31e73ea4e828eea210511bd958124eb9389
Gniare <niare09@hotmail.fr>
2024-03-14 15:04:20 +0100

    2-calloc.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,52 +1,33 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-*string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
-*@s1: string 1
-*@s2: string 2
-*@n: n bytes to concat from string 2
-*Return: pointer to concatenated string
+*_calloc - allocates memory for an array
+*@nmemb: size
+*@size: sizeof(datatype)
+*Return: pointer to calloc'd string
 */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int i = 0, j = 0, k = 0, l = 0;
-char *str;
+void *ptr;
+unsigned int i; /* match unsigned arguments */
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-
-while (s1[i])
-i++;
-
-while (s2[k])
-k++;
-
-if (n >= k)
-l = i + k;
-else
-l = i + n;
-
-str = malloc(sizeof(char) * l + 1);
-if (str == NULL)
+if (nmemb <= 0 || size <= 0) /* validate input */
+{
 return (NULL);
-
-k = 0;
-while (j < l)
-{
-if (j <= i)
-str[j] = s1[j];
-
-if (j >= i)
-{
-str[j] = s2[k];
-k++;
 }
-j++;
+
+ptr = malloc(nmemb * size);
+if (ptr == NULL)
+{
+return (NULL);
 }
-str[j] = '\0';
-return (str);
+
+for (i = 0; i < nmemb * size; i++)
+{
+*((char *)ptr + i) = 0;
+}
+return (ptr);
 }
