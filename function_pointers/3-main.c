5879f3c6d0b213cde2b4b460853a163ca1ceda7b
Gniare <niare09@hotmail.fr>
2024-03-21 18:13:40 +0100

    3-main.c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int (*opera)(int, int);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	opera = get_op_func(argv[2]);
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+	if ((opera == NULL) || (argv[2][1]))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", opera(i, j));
+	return (0);
+}
