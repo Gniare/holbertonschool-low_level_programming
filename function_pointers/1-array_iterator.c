4c1c8fd621c0b0490de94c671ca61ecb732d38fb
Gniare <niare09@hotmail.fr>
2024-03-21 09:32:43 +0100

    File:

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,10 +2,10 @@
 #include "function_pointers.h"
 /**
 *array_iterator - that takes an array of integers
-*@size:The size of the array.
-*@array:A pointer to an integer array.
-*@action:A pointer to a function that takes an integer as an argumen
-*Return:void
+*@size: The size of the array.
+*@array: A pointer to an integer array.
+*@action: A pointer to a function that takes an integer as an argumen
+*Return: void
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
