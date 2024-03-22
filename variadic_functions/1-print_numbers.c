72dd87b460572db3aeea3f2a2b82deecbcd78944
Gniare <niare09@hotmail.fr>
2024-03-22 15:29:57 +0100

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -8,8 +8,9 @@
 *@separator: string to be printed between numbers
 *@n: number of integers passed to the function
 *
-*Return: no return.
+*Return: no return
 */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list valist;
