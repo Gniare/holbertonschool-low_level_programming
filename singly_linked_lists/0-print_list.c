9ac4019310a116045ac3b9842e161ecd4927d41d
Gniare <niare09@hotmail.fr>
2024-04-04 14:12:01 +0200

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <lists.h>
+#include "lists.h"
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -9,12 +9,10 @@
 *
 * Return: the number of nodes
 */
-
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
 const list_t *current = h;
-
 while (current != NULL)
 {
 if (current->str == NULL)
