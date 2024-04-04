018531bbc73609b98fc79f4ef079eb5a3cfe9b42
Gniare <niare09@hotmail.fr>
2024-04-04 13:50:01 +0200

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,32 +4,31 @@
 #include <unistd.h>
 
 /**
- * print_list - print all element of list_t
- * @h: points to the next node
- *
- * Return: the number of nodes
- */
+* print_list - print all element of list_t
+* @h: points to the next node
+*
+* Return: the number of nodes
+*/
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-	const list_t *current = h;
+size_t count = 0;
+const list_t *current = h;
 
-		while (current != NULL)
-		{
-			if (current->str == NULL)
-			{
-				printf("[0] (nil)\n");
-			}
-
-			else
-			{
-				printf("[%u] %s\n", current->len, current->str);
-			}
-			{
-				current = current->next;
-				count++;
-			}
-		}
-		return (count);
+while (current != NULL)
+{
+if (current->str == NULL)
+{
+printf("[%d] %s\n", 0, "(nil)");
+}
+else
+{
+printf("[%d] %s\n", current->len, current->str);
+}
+{
+current = current->next;
+count++;
+}
+}
+return (count);
 }
