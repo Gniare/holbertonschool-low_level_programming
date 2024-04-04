f0ef7a89f59f7220faea8b73c96d6bd4729be972
Gniare <niare09@hotmail.fr>
2024-04-04 15:32:38 +0200

    4-free_list.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,6 +9,7 @@
  *
  * Return: Address of the new element, or NULL if it failed.
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
     list_t *new_node, *last_node;
@@ -17,12 +18,10 @@ list_t *add_node_end(list_t **head, const char *str)
     if (str == NULL)
         return (NULL);
 
-    /* Allocate memory for new node */
     new_node = malloc(sizeof(list_t));
     if (new_node == NULL)
         return (NULL);
 
-    /* Duplicate the string */
     new_str = strdup(str);
     if (new_str == NULL)
     {
@@ -30,24 +29,20 @@ list_t *add_node_end(list_t **head, const char *str)
         return (NULL);
     }
 
-    /* Assign data to the new node */
     new_node->str = new_str;
     new_node->len = strlen(new_str);
     new_node->next = NULL;
 
-    /* If the list is empty, new node is the head */
     if (*head == NULL)
     {
         *head = new_node;
         return (new_node);
     }
 
-    /* Traverse the list to find the last node */
     last_node = *head;
     while (last_node->next != NULL)
         last_node = last_node->next;
 
-    /* Append the new node to the end of the list */
     last_node->next = new_node;
 
     return (new_node);
