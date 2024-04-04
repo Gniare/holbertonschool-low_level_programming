e88da50dfea46ef14e8f2c574a27a26769670e7d
Gniare <niare09@hotmail.fr>
2024-04-04 14:17:16 +0200

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,15 +4,15 @@
 #include <unistd.h>
 
 /**
-* print_list - print all element of list_t
-* @h: points to the next node
-*
-* Return: the number of nodes
+*print_list - print all element of list_t
+*@h: points to the next node
+*Return: the number of nodes
 */
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
 const list_t *current = h;
+
 while (current != NULL)
 {
 if (current->str == NULL)
