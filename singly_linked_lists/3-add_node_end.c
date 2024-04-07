3fdbe73997418488c8c7f809181c9802f282fc27
Gniare <niare09@hotmail.fr>
2024-04-07 12:28:47 +0200

    2-add_node.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -17,21 +17,17 @@ if (str == NULL)
 {
 return (NULL);
 }
-
 new_node = malloc(sizeof(list_t));
-
 if (new_node == NULL)
 {
 return (NULL);
 }
-
 new_node->str = strdup(str);
 if (new_node->str == NULL)
 {
 free(new_node);
 return (NULL);
 }
-
 new_node->len = strlen(str);
 new_node->next = NULL;
 
@@ -47,6 +43,5 @@ while (temp->next != NULL)
 temp = temp->next;
 temp->next = new_node;
 }
-
 return (new_node);
 }
