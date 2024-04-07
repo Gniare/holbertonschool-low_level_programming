1f9df53fbfa1351930d9f8efa2c86db2db782ddc
Gniare <niare09@hotmail.fr>
2024-04-07 12:17:09 +0200

    2-add_node.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,49 +1,52 @@
+#include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-#include "lists.h"
 
 /**
- * add_node_end - Adds a new node at the end of a list_t list.
- * @head: Pointer to a pointer to the head of the list.
- * @str: String to be duplicated and stored in the new node.
- *
- * Return: Address of the new element, or NULL if it failed.
- */
+* add_node_end - adds new node at the end of a list
+* @head: pointer to a pointer to the start of the list
+* @str: string to be duplicated and added to the new node
+* Return: str needs to be duplicated
+*/
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *last_node;
-    char *new_str;
+list_t *new_node;
 
-    if (str == NULL)
-        return (NULL);
+if (str == NULL)
+{
+return (NULL);
+}
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(list_t));
 
-    new_str = strdup(str);
-    if (new_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+if (new_node == NULL)
+{
+return (NULL);
+}
 
-    new_node->str = new_str;
-    new_node->len = strlen(new_str);
-    new_node->next = NULL;
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+new_node->len = strlen(str);
+new_node->next = NULL;
 
-    last_node = *head;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
+if (*head == NULL)
+{
+*head = new_node;
+}
+else
+{
+list_t *temp = *head;
 
-    last_node->next = new_node;
+while (temp->next != NULL)
+temp = temp->next;
+temp->next = new_node;
+}
 
-    return (new_node);
+return (new_node);
 }
--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,18 +2,19 @@
 #include "lists.h"
 
 /**
- * free_list - Frees a linked list
- * @head: Pointer to the head of the list
- */
+* free_list - Frees a linked list
+* @head: Pointer to the head of the list
+*/
+
 void free_list(list_t *head)
 {
-    list_t *temp;
+list_t *temp;
 
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp->str);
-        free(temp);
-    }
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp->str);
+free(temp);
+}
 }
