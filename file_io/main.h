aae588cc54769e6b2d4fbe44f4047d7915d5fd2e
Gniare <niare09@hotmail.fr>
2024-04-18 15:03:12 +0200

    0-read_textfile.c

---
+++ file_io/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void copy_file(const char *src, const char *dest);
+
+int _strlen(char *s);
+
+int _putchar(char c);
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
