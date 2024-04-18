35fa495a4e836a228bea8e16bdb9bba3600b403d
Gniare <niare09@hotmail.fr>
2024-04-18 12:24:06 +0200

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,49 +1,39 @@
 #include "lists.h"
 #include <string.h>
 #include <stdlib.h>
-
 /**
- * add_node - Add a new node at the beginning of list_t
- * @head: Head
- * @str: String
- * Return: list_t
+ * add_node - function that adds a new node at
+ * the beginning of a list_t list.
+ * @head: pointer to a pointer to a list structure.
+ * @str: string of characters
+ *
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-
-	if (head == NULL || str == NULL)
-		return (NULL);
+	size_t length = 0;
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
 
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	while (str[length])
+	{
+		length++;
+	}
+	new_node->len = length;
 	new_node->str = strdup(str);
+
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = _strlen(new_node->str);
 	new_node->next = *head;
 	*head = new_node;
+
 	return (new_node);
-}
-
-/**
- * _strlen - Return length of a string
- * @s: (int) text
- * Return: (int) length
- */
-int _strlen(char *s)
-{
-	int i;
-	int length = 0;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		length++;
-	}
-	return (length);
 }
